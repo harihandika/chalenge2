c71e807991fc2d483c351b5ea5539502f62b476a
unknown <harisnowpop@gmail.com>
2022-09-15 20:50:25 +0700

    chalenge

--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import "fmt"
 
-func main() {
+var bill int
+var discount int
+var DiscountPrice int
+var afterDiscount int
+var voucher string
 
-  var bill int
-  var discount int
-  var resultdiscount int
-  var price int
-  var afterDiscount int
-  fmt.Println("Enter bill amount:")
-  fmt.Scanf("%d", &bill)
-  if bill >= 50000 && bill < 70000{
-	discount = 25
-  } else if bill >= 70000{
-	discount = 50
-  }else if bill < 50000{
-	discount = 0
-  }
-resultdiscount= (discount / 100)
-  fmt.Println(resultdiscount)
- price = (bill * resultdiscount)
-  if  discount == 25 && price >= 20000{
-	price = 20000
-  } else if discount == 50 && price >= 45000{
-	price = 45000
+func DumbWMerchBerkah(){
+  discount = 25
+  DiscountPrice = (bill*discount/100)
+  if  DiscountPrice >= 20000{
+    DiscountPrice=20000
   }else{
-	price = (bill * discount / 100)
+    DiscountPrice = (bill*discount/100)
   }
-//   fmt.Println("Your discount percentage:")
-//   fmt.Scanf("%d", &discount)
-  afterDiscount = bill - price
-  fmt.Println(price)
+}
+
+func DumbWMerchMurahBanget(){
+  discount = 50
+  DiscountPrice = (bill*discount/100)
+  if  DiscountPrice >= 45000{
+    DiscountPrice=45000
+  }else{
+    DiscountPrice = (bill*discount/100)
+  }
+}
+
+func main() {
+  fmt.Println("Enter bill amount:")
+  fmt.Scanf("%d", &bill, &voucher)
+  if bill >= 50000 && bill < 70000{
+    DumbWMerchBerkah()
+    fmt.Println("Your Voucher Discount: DumbWMerchBerkah" )   
+  } else if bill >= 70000{
+	DumbWMerchMurahBanget()
+  fmt.Println("Your Voucher Discount: DumbWMerchMurahBanget" )   
+  }else if bill < 50000{
+	DiscountPrice = 0
+  }
+  fmt.Println(bill)
+  afterDiscount = bill - DiscountPrice
+  fmt.Println("your discount:",discount,"%")
+  fmt.Println("Discount Price :",DiscountPrice)
   fmt.Println("After discount your bill is: " , afterDiscount)
   }
